1bed13134505df3b2de283deaa1f3029a1a2b218
Miles <miles@synthesis.co.za>
2018-10-23 10:09:23 +0200

    adding logging to rest resource

--- spabackend/src/main/java/au/com/hypothesisconsulting/spachallenge/rest/MailSenderResource.java
+++ spabackend/src/main/java/au/com/hypothesisconsulting/spachallenge/rest/MailSenderResource.java
@@ -3,6 +3,8 @@ package au.com.hypothesisconsulting.spachallenge.rest;
 import au.com.hypothesisconsulting.spachallenge.mail.MailGunSender;
 import au.com.hypothesisconsulting.spachallenge.mail.MailProvider;
 import au.com.hypothesisconsulting.spachallenge.mail.SendGridSender;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api")
 public class MailSenderResource {
+
+    Logger logger = LoggerFactory.getLogger(MailSenderResource.class);
 
     @Autowired
     MailGunSender mailGunSender;
@@ -26,10 +30,12 @@ public class MailSenderResource {
             mailGunSender.sendMail(mail.subject,  mail.body, mail.from, mail.to, mail.cc, mail.bcc);
             return new ResponseEntity<>(new ResponseDTO("Successfully sent mail", "MailGun",false), HttpStatus.OK);
         } catch (Exception e) {
+            logger.error("Failed to send with mailgun, falling over to SendGrid");
             try {
                 sendGridSender.sendMail(mail.subject, mail.body, mail.from, mail.to, mail.cc, mail.bcc);
                 return new ResponseEntity<>(new ResponseDTO("Successfully sent mail","SendGrid",false), HttpStatus.OK);
             } catch (Exception ex) {
+                logger.error("Failed to send with SendGrid");
                 return new ResponseEntity<>(new ResponseDTO("Unable to send email", null,true), HttpStatus.INTERNAL_SERVER_ERROR);
             }
         }
